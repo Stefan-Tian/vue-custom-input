b9ebc56ccd52e602819f719e9dff9b5ab10f0d9b
Stefan Tian <stefan55831@gmail.com>
2020-06-15 20:38:07 +0800

    Learned how to create your own vue directives

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
     <app-switch v-model="dataSwitch" :value="dataSwitch"></app-switch>
+    <p v-highlight:background="'blue'">Color this</p>
+    <p v-highlight:color.delay="'blue'">Color this</p>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    var delay = 0;
+    if (binding.modifiers['delay']) {
+      delay = 3000;
+    }
+
+    setTimeout(() => {
+      if (binding.arg === 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+    // el.style.backgroundColor = binding.value;
+  },
+});
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: (h) => h(App),
+});

d44eea86229244e46095c86d9b8b1bba15450003
Stefan Tian <stefan55831@gmail.com>
2020-06-15 20:20:10 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <app-switch v-model="dataSwitch" :value="dataSwitch"></app-switch>
+  </div>
+</template>
+
+<script>
+import Switch from "./Switch.vue";
+export default {
+  data() {
+    return {
+      dataSwitch: true
+    };
+  },
+  components: { "app-switch": Switch }
+};
+</script>
+
+<style>
+</style>
